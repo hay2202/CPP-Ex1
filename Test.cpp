f7f0137c77ca1b8c2761b8246d36ac300e3381fb
user <hayasa229@gmail.com>
2021-03-09 19:05:20 +0200

    part A. Done!

--- Test.cpp
+++ Test.cpp
@@ -26,13 +26,28 @@ string nospaces(string input) {
 
 TEST_CASE("Good snowman code") {
     CHECK(nospaces(snowman(11114411)) == nospaces("_===_\n(.,.)\n( : )\n( : )"));
-    /* Add more checks here */
+    CHECK(snowman(11111111) == "_===_\n(.,.)\n<( : )>\n( : )");
+	CHECK(snowman(44444444) == " ___\n(_*_)\n(- -)\n<(  )>\n(  )");
+	CHECK(snowman(12224411) == "_===_\n(o.o)\n( : )\n( : )");
+	CHECK(snowman(11114433) == "_===_\n(.,.)\n(> <)\n(___)");
+	CHECK(snowman(11114421) == "_===_\n(.,.)\n(] [)\n( : )");
+	CHECK(snowman(11112211) == "_===_\n'\'(.,.)/\n( : )\n( : )");
+	CHECK(snowman(13344411) == "_===_\n(O_-)\n( : )\n( : )");
+	CHECK(snowman(11114412) == "_===_\n(.,.)\n( : )\n(" ")");
+	CHECK(snowman(11114442) == "_===_\n(.,.)\n(   )\n(" ")");
+	CHECK(snowman(11114434) == "_===_\n(.,.)\n(> <)\n(  )");
+	CHECK(snowman(14114411) == "_===_\n(. .)\n( : )\n( : )");
 }
 
 TEST_CASE("Bad snowman code") {
     CHECK_THROWS(snowman(555));
-    /* Add more checks here */
+	CHECK_THROWS(snowman(111111111));
+	CHECK_THROWS(snowman(011111111));
+	CHECK_THROWS(snowman(12345678));
+	CHECK_THROWS(snowman(11023344));
+	CHECK_THROWS(snowman(11111110));
+	CHECK_THROWS(snowman(12345123));
+	CHECK_THROWS(snowman(4444444));
+    
 }
 
-
-/* Add more test cases here */
--- snowman.cpp
+++ snowman.cpp
@@ -5,13 +5,26 @@ using namespace std;
 #include <stdexcept>
 #include <string>
 
+   int validInput(int n){
+        while (n>0)
+        {
+            int c = n%10;
+            n = n/10;
+            if (c < 1 || c > 4)
+                return -1;
+        }   
+        return 1;
+    }
+
 namespace ariel {
 	string snowman(int x) {
         if (x < 11111111 || x > 44444444)
         {
-            throw std::out_of_range{"x must be in the range"};
+            throw std::out_of_range{"invalid input!!\n"};
         }
-        
+        if (validInput(x) == -1){
+            throw std::out_of_range{"invalid input!!\n"};
+        }
         return"_===_ \n(.,.)\n( : )\n( : )\n";
 
 	}
